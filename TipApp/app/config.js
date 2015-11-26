ba06fdd017a9b7326a6f861bdc64ed0718ad8c33
Richard Lee <lifuzu@gmail.com>
2015-11-25 16:50:09 -0800

    Point to formal server address.

--- TipApp/app/config.js
+++ TipApp/app/config.js
@@ -3,8 +3,8 @@
 module.exports = {
   "Settings": {
     // "BASE_URL":                     "http://weimed.com:3100/",
-    "BASE_URL":                     "http://192.168.99.100:8080/api",
-    "DB_BASE_URL":                  "http://localhost:3000/",
+    // "BASE_URL":                     "http://192.168.99.100:8080/api",
+    "BASE_URL":                     "http://lab.weimed.com:8090/api",
     "ACTIONS_UPDATE_CACHED_DATA":   true
   }
 }
