fc503393ab9a6cde38e0c729ac27da733122a20f
Richard Lee <lifuzu@gmail.com>
2015-11-27 19:50:05 -0800

    Added a temp + right button in ScanView for developing.

--- TipApp/app/components/ScanView.js
+++ TipApp/app/components/ScanView.js
@@ -92,7 +92,15 @@ var NavigationBarRouteMapper = {
     );
   },
   RightButton(route, navigator, index, nextState) {
-    return null;
+    return (
+      <TouchableOpacity style={{flex: 1, justifyContent: 'center'}}
+          onPress={() => navigator.parentNavigator.push({id: 'detail', params: {data: 'menu-item/chicken-fried-steak/'}})}>
+        <Icon
+          name='ion|ios-plus'
+          size={40}
+          color='#887700'
+          style={{width: 40, height: 40}} />
+      </TouchableOpacity>);
   },
   Title(route, navigator, index, nextState) {
     return (
