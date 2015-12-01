a3504c847b8963809537606bfcd298402428d50e
Richard Lee <lifuzu@gmail.com>
2015-12-01 09:24:26 -0800

    Keep Android happy without Geolocation support yet.

--- TipApp/app/mixins/geolocation.js
+++ TipApp/app/mixins/geolocation.js
@@ -1,6 +1,7 @@
 // geolocation.js
 'use strict';
 var React = require('react-native');
+var { Platform, } = React;
 
 var Geolocation = {
   watchID: (null: ?number),
@@ -13,18 +14,24 @@ var Geolocation = {
   },
 
   componentDidMount: function() {
-    navigator.geolocation.getCurrentPosition(
-      (initialPosition) => this.setState({initialPosition}),
-      (error) => console.error(error.message),
-      {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
-    );
-    this.watchID = navigator.geolocation.watchPosition((lastPosition) => {
-      this.setState({lastPosition});
-    });
+    // TODO: Support Android later to upgrade React Native to > v0.16.0
+    // OR http://stackoverflow.com/questions/33341760/react-native-android-geolocation
+    if (Platform.OS === 'ios') {
+      navigator.geolocation.getCurrentPosition(
+        (initialPosition) => this.setState({initialPosition}),
+        (error) => console.error(error.message),
+        {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
+      );
+      this.watchID = navigator.geolocation.watchPosition((lastPosition) => {
+        this.setState({lastPosition});
+      });
+    }
   },
 
   componentWillUnmount: function() {
-    navigator.geolocation.clearWatch(this.watchID);
+    if (Platform.OS === 'ios') {
+      navigator.geolocation.clearWatch(this.watchID);
+    }
   },
 
 };
