9c97ae2a9e029b5269b383d298be0d2d1122fed4
Richard Lee <lifuzu@gmail.com>
2015-12-02 20:39:08 -0800

    Support navigation back from conversation view.

--- TipApp/app/views/ConversationView.js
+++ TipApp/app/views/ConversationView.js
@@ -6,6 +6,7 @@ var React = require('react-native');
 var {
   StyleSheet,
   Component,
+  Dimensions,
   View,
   Text,
   TextInput,
@@ -15,13 +16,13 @@ var {
 } = React;
 
 var GiftedMessenger = require('react-native-gifted-messenger');
-var {Dimensions} = React;
+var { Icon, } = require('react-native-icons');
 
 var ConversationView = React.createClass({
   getInitialMessages() {
     return [
-      {text: 'Are you building a chat app?', name: 'React-Native', image: {uri: 'https://facebook.github.io/react/img/logo_og.png'}, position: 'left', date: new Date(2015, 0, 16, 19, 0)},
-      {text: "Yes, and I use Gifted Messenger!", name: 'Developer', image: null, position: 'right', date: new Date(2015, 0, 17, 19, 0)},
+      {text: 'Are you building a food order app?', name: 'ABC Restaurant', image: {uri: 'https://facebook.github.io/react/img/logo_og.png'}, position: 'left', date: new Date(2015, 10, 16, 19, 0)},
+      {text: "Yes, and I use Fast Order!", name: 'Developer', image: {uri: 'https://facebook.github.io/react/img/logo_og.png'}, position: 'right', date: new Date(2015, 10, 17, 19, 0)},
     ];
   },
   handleSend(message = {}, rowID = null) {
@@ -38,12 +39,23 @@ var ConversationView = React.createClass({
   },
   render() {
     return (
+      <Navigator
+        renderScene={this._renderScene}
+        navigator={this.props.navigator}
+        navigationBar={
+          <Navigator.NavigationBar style={{backgroundColor: '#246dd5'}}
+            routeMapper={NavigationBarRouteMapper} />
+        } />
+    );
+  },
+  _renderScene() {
+    return (
       <GiftedMessenger
         ref={(c) => this._GiftedMessenger = c}
 
         initialMessages={this.getInitialMessages()}
         handleSend={this.handleSend}
-        maxHeight={Dimensions.get('window').height - 64} // 64 for the navBar
+        maxHeight={Dimensions.get('window').height - 64 - 30} // 64 for the navBar
 
         styles={{
           bubbleLeft: {
@@ -58,5 +70,31 @@ var ConversationView = React.createClass({
     );
   },
 });
+
+var NavigationBarRouteMapper = {
+  LeftButton(route, navigator, index, nextState) {
+    return (
+      <TouchableOpacity style={{flex: 1, justifyContent: 'center'}}
+          onPress={() => navigator.parentNavigator.pop()}>
+        <Icon
+          name='ion|ios-arrow-back'
+          size={40}
+          color='#887700'
+          style={{width: 40, height: 40}} />
+      </TouchableOpacity>
+    );
+  },
+  RightButton(route, navigator, index, nextState) {
+    return null;
+  },
+  Title(route, navigator, index, nextState) {
+    return (
+      <Text style={{color: 'white', margin: 10, fontSize: 16}}>
+        Conversations
+      </Text>
+    );
+  }
+};
+
 
 module.exports = ConversationView;
