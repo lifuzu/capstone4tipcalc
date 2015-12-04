f154bbf87b32b52ceac7c58925e79054ead2e70a
Richard Lee <lifuzu@gmail.com>
2015-12-03 17:36:56 -0800

    Support CI builds.

---
+++ TipApp/tasks/build.js
@@ -0,0 +1,10 @@
+var Compiler = require("./compiler");
+
+var compiler = new Compiler('ios', process.env.TARGET, console);
+compiler.cleanDirectory();
+compiler.build();
+compiler.zip();
+
+if (process.env.PHONE) {
+  compiler.phoneInstall();
+}
