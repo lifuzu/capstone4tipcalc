67e8529e473c1ccfeac2fcb4f5877c752ef2ceb0
Richard Lee <lifuzu@gmail.com>
2015-12-05 21:50:13 -0800

    Fix an Apple issue when packaging app

---
+++ TipApp/tasks/fixups/run.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# http://stackoverflow.com/questions/26497863/xcode-6-1-error-while-building-ipa/26499526#26499526
+cp "$DIR/PackageApplication" "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/usr/bin/PackageApplication"
