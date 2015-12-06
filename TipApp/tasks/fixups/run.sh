2a1257f5167d64aee798ab3d8bb88f79a95ebaad
Richard Lee <lifuzu@gmail.com>
2015-12-05 22:11:15 -0800

    Replace the modified PackageApplication solution with adding an option to xctool

--- TipApp/tasks/fixups/run.sh
+++ TipApp/tasks/fixups/run.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # http://stackoverflow.com/questions/26497863/xcode-6-1-error-while-building-ipa/26499526#26499526
+# http://cutting.io/posts/packaging-ios-apps-from-the-command-line/
 cp "$DIR/PackageApplication" "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/usr/bin/PackageApplication"
