604ef0056a60f7dd64b09ad04f7eadb44beb6675
Richard Lee <lifuzu@gmail.com>
2015-12-09 16:12:05 -0800

    Support to check in changes for version.

--- TipApp/tasks/commit-all-changes.sh
+++ TipApp/tasks/commit-all-changes.sh
@@ -4,6 +4,15 @@
 VERSION=`agvtool what-marketing-version | grep Found | cut -d ' ' -f 4 | tr -d '"'`
 BUILD=`agvtool what-version | grep -E '\d+' | cut -d ' ' -f 5`
 
+# Config git user
+git config --global user.email "lifuzu@yahoo.com"
+git config --global user.name "lifuzu"
+
+# Reset origin with authentication credentials
+# http://stackoverflow.com/questions/19845679/build-with-travis-ci-and-push-some-files-folder-to-another-repository
+git remote rm origin
+git remote add origin https://lifuzu:${GH_TOKEN}@github.com/lifuzu/capstone4tipcalc.git
+
 #[ -z "$1" ] && ( echo 'Please input a comment.'; exit 1;)
 # Add all changes which should be checked in soon
 git add .
